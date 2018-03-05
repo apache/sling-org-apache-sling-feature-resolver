568557a62e33d52a7ffc5582d75e45bfe4002148
David Bosschaert <bosschae@adobe.com>
2018-03-05 15:05:23 +0000

    SLING-7521 Order bundles in the generated app based on feature order and start order
    Order resource (bundles and features) in the resulting application based on the order
    of resolved features and then also in the order of the start order within the feature.

--- src/main/java/org/apache/sling/feature/resolver/impl/BundleResourceImpl.java
+++ src/main/java/org/apache/sling/feature/resolver/impl/BundleResourceImpl.java
@@ -16,14 +16,9 @@
  */
 package org.apache.sling.feature.resolver.impl;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import org.apache.sling.feature.Artifact;
+import org.apache.sling.feature.Feature;
+import org.apache.sling.feature.FeatureResource;
 import org.apache.sling.feature.OSGiCapability;
 import org.apache.sling.feature.OSGiRequirement;
 import org.apache.sling.feature.analyser.BundleDescriptor;
@@ -32,25 +27,38 @@ import org.osgi.framework.Version;
 import org.osgi.framework.VersionRange;
 import org.osgi.framework.namespace.BundleNamespace;
 import org.osgi.framework.namespace.ExecutionEnvironmentNamespace;
+import org.osgi.framework.namespace.IdentityNamespace;
 import org.osgi.framework.namespace.PackageNamespace;
 import org.osgi.resource.Capability;
 import org.osgi.resource.Requirement;
-import org.osgi.resource.Resource;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Implementation of the OSGi Resource interface.
  */
-public class BundleResourceImpl implements Resource {
-    final String hint;
+public class BundleResourceImpl extends AbstractResourceImpl implements FeatureResource {
+    final Artifact artifact;
+    final String bsn;
+    final Version version;
     final Map<String, List<Capability>> capabilities;
     final Map<String, List<Requirement>> requirements;
+    final Feature feature;
 
     /**
      * Create a resource based on a BundleDescriptor.
      * @param bd The BundleDescriptor to represent.
      */
-    public BundleResourceImpl(BundleDescriptor bd) {
-        hint = bd.getBundleSymbolicName() + " " + bd.getBundleVersion();
+    public BundleResourceImpl(BundleDescriptor bd, Feature feat) {
+        artifact = bd.getArtifact();
+        bsn = bd.getBundleSymbolicName();
+        version = bd.getArtifact().getId().getOSGiVersion();
+        feature = feat;
+
         Map<String, List<Capability>> caps = new HashMap<>();
         for (Capability c : bd.getCapabilities()) {
             List<Capability> l = caps.get(c.getNamespace());
@@ -67,16 +75,24 @@ public class BundleResourceImpl implements Resource {
             Map<String, Object> attrs = new HashMap<>();
             attrs.put(PackageNamespace.PACKAGE_NAMESPACE, exported.getName());
             attrs.put(PackageNamespace.CAPABILITY_VERSION_ATTRIBUTE, exported.getPackageVersion());
-            attrs.put(PackageNamespace.CAPABILITY_BUNDLE_SYMBOLICNAME_ATTRIBUTE, bd.getBundleSymbolicName());
-            attrs.put(PackageNamespace.CAPABILITY_BUNDLE_VERSION_ATTRIBUTE, new Version(bd.getBundleVersion()));
+            attrs.put(PackageNamespace.CAPABILITY_BUNDLE_SYMBOLICNAME_ATTRIBUTE, bsn);
+            attrs.put(PackageNamespace.CAPABILITY_BUNDLE_VERSION_ATTRIBUTE, version);
             pkgCaps.add(new OSGiCapability(this, PackageNamespace.PACKAGE_NAMESPACE, attrs, Collections.emptyMap()));
         }
         caps.put(PackageNamespace.PACKAGE_NAMESPACE, Collections.unmodifiableList(pkgCaps));
 
+        // Add the identity capability
+        Map<String, Object> idattrs = new HashMap<>();
+        idattrs.put(IdentityNamespace.IDENTITY_NAMESPACE, bsn);
+        idattrs.put(IdentityNamespace.CAPABILITY_TYPE_ATTRIBUTE, IdentityNamespace.TYPE_BUNDLE);
+        idattrs.put(IdentityNamespace.CAPABILITY_VERSION_ATTRIBUTE, version);
+        OSGiCapability idCap = new OSGiCapability(this, IdentityNamespace.IDENTITY_NAMESPACE, idattrs, Collections.emptyMap());
+        caps.put(IdentityNamespace.IDENTITY_NAMESPACE, Collections.singletonList(idCap));
+
         // Add the bundle capability
         Map<String, Object> battrs = new HashMap<>();
-        battrs.put(BundleNamespace.BUNDLE_NAMESPACE, bd.getBundleSymbolicName());
-        battrs.put(BundleNamespace.CAPABILITY_BUNDLE_VERSION_ATTRIBUTE, new Version(bd.getBundleVersion()));
+        battrs.put(BundleNamespace.BUNDLE_NAMESPACE, bsn);
+        battrs.put(BundleNamespace.CAPABILITY_BUNDLE_VERSION_ATTRIBUTE, version);
         OSGiCapability bundleCap = new OSGiCapability(this, BundleNamespace.BUNDLE_NAMESPACE, battrs, Collections.emptyMap());
         caps.put(BundleNamespace.BUNDLE_NAMESPACE, Collections.singletonList(bundleCap));
         capabilities = Collections.unmodifiableMap(caps);
@@ -117,48 +133,59 @@ public class BundleResourceImpl implements Resource {
         requirements = Collections.unmodifiableMap(reqs);
     }
 
-    /**
-     * Constructor. Create a resource based on capabilties and requirements.
-     * @param hnt
-     * @param caps The capabilities of the resource.
-     * @param reqs The requirements of the resource.
-     */
-    public BundleResourceImpl(Map<String, List<Capability>> caps, Map<String, List<Requirement>> reqs) {
-        hint = "" + System.identityHashCode(this);
+    public BundleResourceImpl(String sn, String ver, Artifact art, Feature feat, Map<String, List<Capability>> caps, Map<String, List<Requirement>> reqs) {
+        this(sn, new Version(ver), art, feat, caps, reqs);
+    }
+
+    public BundleResourceImpl(String sn, Version ver, Artifact art, Feature feat, Map<String, List<Capability>> caps, Map<String, List<Requirement>> reqs) {
+        artifact = art;
+        bsn = sn;
+        version = ver;
+        feature = feat;
         capabilities = caps;
         requirements = reqs;
     }
 
     @Override
-    public List<Capability> getCapabilities(String namespace) {
-        if (namespace == null) {
-            return capabilities.values().stream().flatMap(Collection::stream).collect(Collectors.toList());
-        }
+    public Artifact getArtifact() {
+        return artifact;
+    }
 
-        List<Capability> caps = capabilities.get(namespace);
-        if (caps == null)
-            return Collections.emptyList();
-        return caps;
+    @Override
+    public String getId() {
+        return bsn;
+    }
+
+    @Override
+    public Version getVersion() {
+        return version;
+    }
+
+    @Override
+    public List<Capability> getCapabilities(String namespace) {
+        return super.getCapabilities(namespace, capabilities);
     }
 
     @Override
     public List<Requirement> getRequirements(String namespace) {
-        if (namespace == null) {
-            return requirements.values().stream().flatMap(Collection::stream).collect(Collectors.toList());
-        }
+        return super.getRequirements(namespace, requirements);
+    }
 
-        List<Requirement> reqs = requirements.get(namespace);
-        if (reqs == null)
-            return Collections.emptyList();
-        return reqs;
+    @Override
+    public Feature getFeature() {
+        return feature;
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
+        result = prime * result + ((artifact == null) ? 0 : artifact.hashCode());
+        result = prime * result + ((bsn == null) ? 0 : bsn.hashCode());
         result = prime * result + ((capabilities == null) ? 0 : capabilities.hashCode());
+        result = prime * result + ((feature == null) ? 0 : feature.hashCode());
         result = prime * result + ((requirements == null) ? 0 : requirements.hashCode());
+        result = prime * result + ((version == null) ? 0 : version.hashCode());
         return result;
     }
 
@@ -171,21 +198,41 @@ public class BundleResourceImpl implements Resource {
         if (getClass() != obj.getClass())
             return false;
         BundleResourceImpl other = (BundleResourceImpl) obj;
+        if (artifact == null) {
+            if (other.artifact != null)
+                return false;
+        } else if (!artifact.equals(other.artifact))
+            return false;
+        if (bsn == null) {
+            if (other.bsn != null)
+                return false;
+        } else if (!bsn.equals(other.bsn))
+            return false;
         if (capabilities == null) {
             if (other.capabilities != null)
                 return false;
         } else if (!capabilities.equals(other.capabilities))
+            return false;
+        if (feature == null) {
+            if (other.feature != null)
+                return false;
+        } else if (!feature.equals(other.feature))
             return false;
         if (requirements == null) {
             if (other.requirements != null)
                 return false;
         } else if (!requirements.equals(other.requirements))
             return false;
+        if (version == null) {
+            if (other.version != null)
+                return false;
+        } else if (!version.equals(other.version))
+            return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "BundleResourceImpl [" + hint + "]";
+        return "BundleResourceImpl [bsn=" + bsn + ", version=" + version + "]";
     }
 }
